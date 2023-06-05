7ace53b249059494f7f400b77283bbf7124b216e
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-06-06 00:26:16 +0600

    2

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle subHeading="please provide" heading="payment">
+                
+            </SectionTitle>
+            <h2>tk</h2>
+        </div>
+    );
+};
+
+export default Payment;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -15,6 +15,7 @@ import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 import AddItem from "../Pages/Dashboard/AddItem/Additem";
 import AdminRoute from "./AdminRoute";
 import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
+import Payment from "../Pages/Dashboard/Payment/Payment";
 
   export const router = createBrowserRouter([
     {
@@ -57,8 +58,13 @@ import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
           element: <MyCart></MyCart>
         },
         {
+          path: 'payment',
+          element: <Payment></Payment>
+        },
+        //admin route
+        {
           path: 'allusers',
-          element: <AllUsers></AllUsers>
+          element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
         },
         {
           path: 'addItem',
