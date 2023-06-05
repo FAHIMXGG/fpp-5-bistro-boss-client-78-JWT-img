fa3c799aabafcb2590b671f9fb3ef981fa843242
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-06-05 11:34:39 +0600

    7 img

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -5,13 +5,16 @@ import Swal from "sweetalert2";
 
 const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 
+
 const AddItem = () => {
+    //https://api.imgbb.com/1/upload?key
+    
     const [axiosSecure] = useAxiosSecure();
-    const { register, handleSubmit, reset } = useForm();
-    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+    const { register, handleSubmit,  } = useForm();
+    const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
 
     const onSubmit = data => {
-        
+        console.log(data)
         const formData = new FormData();
         formData.append('image', data.image[0])
 
@@ -21,31 +24,32 @@ const AddItem = () => {
         })
         .then(res => res.json())
         .then(imgResponse => {
+            console.log(imgResponse)
             if(imgResponse.success){
                 const imgURL = imgResponse.data.display_url;
                 const {name, price, category, recipe} = data;
                 const newItem = {name, price: parseFloat(price), category, recipe, image:imgURL}
                 console.log(newItem)
-                axiosSecure.post('/menu', newItem)
-                .then(data => {
-                    console.log('after posting new menu item', data.data)
-                    if(data.data.insertedId){
-                        reset();
-                        Swal.fire({
-                            position: 'top-end',
-                            icon: 'success',
-                            title: 'Item added successfully',
-                            showConfirmButton: false,
-                            timer: 1500
-                          })
-                    }
-                })
+                // axiosSecure.post('/menu', newItem)
+                // .then(data => {
+                //     console.log('after posting new menu item', data.data)
+                //     if(data.data.insertedId){
+                //         reset();
+                //         Swal.fire({
+                //             position: 'top-end',
+                //             icon: 'success',
+                //             title: 'Item added successfully',
+                //             showConfirmButton: false,
+                //             timer: 1500
+                //           })
+                //     }
+                // })
             }
         })
 
     };
     
-    
+    console.log(img_hosting_token)
     return (
         <div className="w-full px-10">
             <SectionTitle subHeading="What's new" heading="Add an item" ></SectionTitle>
